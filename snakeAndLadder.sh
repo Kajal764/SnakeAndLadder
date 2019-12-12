7dacd2e159002ad4c6d1518d1d6b8f7fae159b73
kajal764 <kajaldw666@gmail.com>
2019-12-12 16:49:30 +0530

    Added: Random Function For Next Move

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -2,10 +2,38 @@
 
 echo "!!!!! Welcome To Snake And Ladder Game !!!!!"
 
+#constant
+START=0
+NOPLAY=0
+SNAKE=1
+LADDER=2
+
 #variable
 position=0
 
 
-echo die=$(($((RANDOM%6))+1))
+function movePosition(){
+	die=$(($((RANDOM%6))+1))
+	move=$(($RANDOM%3))
 
+	case $move in
+	$NOPLAY)
+		echo "No Move"
+	;;
+	$SNAKE)
+		echo "Snake Move"
+		position=$(( $poistion - $die ))
+		if [[ $position -lt 0 ]]
+		then
+			position=$START
+		fi
+	;;
+	$LADDER)
+		echo "Ladder Move"
+		position=$(($poistion+$die))
+	;;
+        esac
+	 echo $position
 
+}
+movePosition
