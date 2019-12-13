dee7ceea3bb2e876f9dbfa83ceac2f9c9301ab99
kajal764 <kajaldw666@gmail.com>
2019-12-13 18:31:26 +0530

    Added: While Loop To Reapeat die Till Player Win

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -13,27 +13,34 @@ position=0
 
 
 function movePosition(){
-	die=$(($((RANDOM%6))+1))
-	move=$(($RANDOM%3))
+	while [[  $position -ne 100 ]]
+	do
+		die=$(($((RANDOM%6))+1))
+		move=$(($RANDOM%3))
 
-	case $move in
-	$NOPLAY)
-		echo "No Move"
-	;;
-	$SNAKE)
-		echo "Snake Move"
-		position=$(( $poistion - $die ))
-		if [[ $position -lt 0 ]]
-		then
-			position=$START
-		fi
-	;;
-	$LADDER)
-		echo "Ladder Move"
-		position=$(($poistion+$die))
-	;;
-        esac
-	 echo $position
+		case $move in
+		$NOPLAY)
+			echo "No Move"
+		;;
+		$SNAKE)
+			echo "Snake Move"
+			position=$(( $position - $die ))
+			if [[ $position -lt 0 ]]
+			then
+				position=$START
+			fi
+		;;
+		$LADDER)
+			echo "Ladder Move"
+			position=$(( $position + $die ))
+			if [[ $position -gt 100 ]]
+			then
+				position=$(( $position - $die ))
+			fi
+		;;
+		esac
+	 	echo $position
+	done
 
 }
 movePosition
