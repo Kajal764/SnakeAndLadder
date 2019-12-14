34389517b86080b095205941b4c2f0b3e0c9a8bf
kajal764 <kajaldw666@gmail.com>
2019-12-14 15:12:18 +0530

    Added: getWinningPlayer function to play for 2 player

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -10,39 +10,60 @@ LADDER=2
 WINNING_POSITION=100
 
 #variable
+positionOfPlayer1=0
+positionOfPlayer2=0
+player=1
 position=0
-noOfDie=0
 
-function movePosition(){
-	while [[  $position -ne $WINNING_POSITION ]]
+function getPosition(){
+	position=$1
+	die=$(($((RANDOM%6))+1))
+	move=$(($RANDOM%3))
+	case $move in
+	$NOPLAY)
+	;;
+	$SNAKE)
+		if [ $(( $position - $die )) -ge $START ]
+		then
+			  position=$(( $position - $die ))
+		fi
+	;;
+	$LADDER)
+		if [[ $(($position + $die)) -le $WINNING_POSITION ]]
+		then
+			position=$(($position + $die))
+		fi
+	;;
+	esac
+
+	echo $position
+}
+
+
+function getWinningPlayer(){
+	while [[  $positionOfPlayer1 -lt $WINNING_POSITION && $positionOfPlayer2 -lt $WINNING_POSITION ]]
 	do
-		die=$(($((RANDOM%6))+1))
-		echo "die value $die"
-		move=$(($RANDOM%3))
-
-		case $move in
-		$NOPLAY)
-			echo "No Move"
-		;;
-		$SNAKE)
-			echo "Snake Move"
-			if [ $(( $position - $die )) -ge $START ]
-			then
-				  position=$(( $position - $die ))
-			fi
-		;;
-		$LADDER)
-			echo "Ladder Move"
-			if [[ $(($position + $die)) -le $WINNING_POSITION ]]
-			then
-				position=$(($position + $die))
-			fi
-		;;
-		esac
-		dieRoll=$(($dieRoll+1))
-		echo "dieRoll $dieRoll"
-	 	echo "position $position"
+		if [[ $player -eq 1 ]]
+		then
+			echo "player 1"
+			position=$(getPosition $positionOfPlayer1 )
+			positionOfPlayer1=$position
+			echo  "positionOfPlayer1 $positionOfPlayer1"
+			player=2
+		else
+			echo "player 2"
+			position=$(getPosition $positionOfPlayer2)
+			positionOfPlayer2=$position
+         echo  "positionOfPlayer2 $positionOfPlayer2"
+			player=1
+		fi
 	done
 
+	if [[ $positionOfPlayer1 -eq $WINNING_POSITION ]]
+	then
+		echo "WINNER IS PLAYER 1 !!!"
+	else
+		echo "WINNER IS PLAYER 2 !!!"
+	fi
 }
-movePosition
+getWinningPlayer
